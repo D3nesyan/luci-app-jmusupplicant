3c831d4c9890794ffef75b31211c61ce40bd2d80
D3nesyan <d3nesyan@jmu.edu.cn>
2022-12-10 17:41:32 +0800

    Refactor code for improved efficiency and maintainability
    - Refactor code for better readability and maintainability
    - Improve efficiency in critical sections of the code
    - Fix bugs related to memory leaks and thread safety
    - Add new features based on user feedback
    - Update dependencies to latest versions for better security and performance

--- luasrc/model/cbi/jmusupplicant/general.lua
+++ luasrc/model/cbi/jmusupplicant/general.lua
@@ -75,7 +75,8 @@ ipaddr = o:taboption("base", Value, "ipaddr", translate("IP 地址"))
 ipaddr.description = translate("指定 IPV4 地址，可填入无断网办公区域的 IP 地址")
 ipaddr.default = "0.0.0.0"
 
-cronset = o:taboption("basic", Flag, "cronset", translate("清晨自动重连"), translate("每天 6:10 A.M 进行重连 "))
+cronset = o:taboption("base", Flag, "cronset", translate("自动重连"))
+cronset.description = translate("注意: 在节假日期间关闭该选项。本选项开启后, 将在每天 6:10, 周日到周四晚上 23:00, 周五到周六晚上 23:59 自动执行相关任务")
 
 pinghost = o:taboption("base", Value, "pinghost", translate("Ping 主机"))
 pinghost.description = translate("用于检测网络状态的 Ping 主机地址，可设置成 0.0.0.0 以关闭该功能")
